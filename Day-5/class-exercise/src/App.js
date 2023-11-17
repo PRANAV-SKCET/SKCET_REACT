301f286ec216ea77185867985065a65547f735f6
Pranav A <124855892+PRANAV-SKCET@users.noreply.github.com>
2023-11-17 20:43:09 +0530

    Add files via upload

---
+++ Day-5/class-exercise/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import List from './components/error';
+
+
+
+
+function App() {
+  return (
+    <>
+    <List/>
+    </> 
+  );
+}
+
+export default App;
+
---
+++ Day-5/practice-at-home/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import MountingPhase from './components/componentTime';
+
+
+
+
+function App() {
+  return (
+    <>
+ <MountingPhase/>
+    </> 
+  );
+}
+
+export default App;
+
