513be63e2647a494e49592d202cb89fee2763287
Pranav A <124855892+PRANAV-SKCET@users.noreply.github.com>
2023-11-23 19:20:18 +0530

    Update error.js

--- Day-5/class-exercise/error.js
+++ Day-5/class-exercise/error.js
@@ -1,18 +1,64 @@
-import React from 'react'
-function Demo(props){
-    return(
-        <div>
-            my name is {props.name}
-        </div>
-    )
+import React, { Component } from "react";
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.error(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <div>Check error message.</div>;
+    }
+
+    return this.props.children;
+  }
 }
-export default function List() {
-    const list=["mango","orange","apple"];
+
+// export default ErrorBoundary;
+
+// App.js
+import React from "react";
+import ErrorBoundary from "./ErrorBoundary";
+import List from "./List";
+
+const App = () => {
+  const data = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"];
+
   return (
     <div>
-        {list.map((temp)=>
-            <Demo name={temp}/>
-        )}
+      <h1>My App</h1>
+      <ErrorBoundary>
+        <List data={data} />
+      </ErrorBoundary>
     </div>
-  )
-}
+  );
+};
+
+export default App;
+
+import React from "react";
+
+const List = ({ data }) => {
+  if (data === null) {
+    throw new Error("Data is null");
+  }
+
+  return (
+    <ul>
+      {data.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
+  );
+};
+
+// export default List;
